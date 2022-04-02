54726e2970c2cd90f579b07a4026c2a7f1c16275
Joao Ortega <joaoortegabr@gmail.com>
2022-04-01 21:24:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let order = [];
 let clickedOrder = [];
 let score = 0;
+let record = localStorage.getItem('record');
+let genius = document.getElementById('genius');
+
+const audio = [
+    new Audio('mp3/1.mp3'),
+    new Audio('mp3/2.mp3'),
+    new Audio('mp3/3.mp3'),
+    new Audio('mp3/4.mp3')
+];
+const audioError = new Audio('mp3/error.mp3');
 
 // 0 = green
 // 1 = red
@@ -11,6 +21,8 @@ const blue = document.querySelector('.blue');
 const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
+const playBtn = document.querySelector('#play');
+const resetBtn = document.querySelector('#reset');
 
 let shuffle = () => {
     let colorOrder = Math.floor(Math.random() * 4);
@@ -19,30 +31,31 @@ let shuffle = () => {
 
     for(let i in order) {
         let elementColor = createColorElement(order[i]);
-        lightColor(elementColor, Number(i) + 1);
+        lightColor(elementColor, Number(i) + 1, order[i]);
     }
 }
 
 function createColorElement(color) {
-    if(color == 0) {
-        return green;
-    } else if(color == 1) {
-        return red;
-    } else if(color == 2) {
-        return yellow;
-    } else if (color == 3) {
-        return blue;
+    switch (color) {
+        case 0: return green;
+        case 1: return red;
+        case 2: return yellow;
+        case 3: return blue;
+        default:    alert('Error. Game must restart.');
+                    location.reload(true);
+                    return;
     }
 }
 
-function lightColor(element, number) {
-    number *= 500;
+function lightColor(elementColor, number, color) {
+    number *= 600;
     setTimeout(() => {
-        element.classList.add('selected')
-    }, number - 250);
+        audio[color].play();
+        elementColor.classList.add('selected');
+    }, number - 400);
     setTimeout(() => {
-        element.classList.remove('selected');
-    }, 100);
+        elementColor.classList.remove('selected');
+    }, number);
 }
 
 function checkOrder() {
@@ -53,7 +66,8 @@ function checkOrder() {
         }
     }
     if(clickedOrder.length == order.length) {
-        alert(`Score: ${score} \nCorrect! Starting next level.`);
+        score++;
+    //    alert(`Correct!\nStarting next level.`);
         nextLevel();
     }
 }
@@ -61,6 +75,7 @@ function checkOrder() {
 function click(color) {
     clickedOrder[clickedOrder.length] = color;
     createColorElement(color).classList.add('selected');
+    audio[color].play();
 
     setTimeout(() => {
         createColorElement(color).classList.remove('selected');
@@ -69,26 +84,62 @@ function click(color) {
 }
 
 function nextLevel() {
-    score++;
     shuffle();
 }
 
 function gameOver() {
-    alert(`Score: ${score} \nYou lost!\nClick OK to start a new game.`);
-    order = [];
-    clickedOrder = [];
-    playGame();
+    audioError.play();
+    disableClick();
+    updateRecord();
+    alert(`Game over.\nYour final score is: ${score} \n`);
+    location.reload(true);
 }
 
 function playGame() {
-    alert(`Welcome do Genius Clone.\nStarting new game.`)
+    alert(`Starting new game.\nGet ready!`);
+    order = [];
+    clickedOrder = [];
     score = 0;
+    enableClick();
     nextLevel();
 }
+
+function updateRecord() {
+    if(score > record) localStorage.setItem('record', score);
+    document.getElementById('record').innerHTML = (record) ? record : 0;
+}
+
+function control(control) {
+    switch (control) {
+        case 'play':
+            playGame();
+            break;
+        case 'reset':
+            localStorage.clear();
+            updateRecord();
+            location.reload(true);
+            break;
+        default:
+            alert('Error. Game must restart.');
+            location.reload(true);
+    }
+}
+
+function disableClick() {
+    genius.classList.add('disabled');
+}
+
+function enableClick() {
+    genius.classList.remove('disabled');
+}
+
 
 green.onclick = () => click(0);
 red.onclick = () => click(1);
 yellow.onclick = () => click(2);
 blue.onclick = () => click(3);
+playBtn.onclick = () => control('play');
+resetBtn.onclick = () => control('reset');
 
-playGame();
+updateRecord();
+
